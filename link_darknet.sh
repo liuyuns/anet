11f57f7c5fbfafc0dcad4aa8ebbeac6658018412
Easton Liu <yun-sheng.liu@hpe.com>
2018-09-10 13:38:33 +0800

    update link_darknet.sh

--- link_darknet.sh
+++ link_darknet.sh
@@ -6,4 +6,8 @@ if [ $# -eq 0 ]
     exit 1
 fi
 
+echo "Link darknet folder to ./darknet"
 ln -s $1 ./darknet 
+
+echo "Link {darknet}/data/lables to ./data/lables"
+ln -s $1/data/labels ./data/labels
