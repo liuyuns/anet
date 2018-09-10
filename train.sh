7f64a89398411fd99924ee6f41224d0d4dd1feb3
Easton Liu <easton.ysliu@gmail.com>
2018-09-10 09:22:50 +0000

    modify train.sh to use gpus, and allow pass conv

--- train.sh
+++ train.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-./darknet/darknet detector train cfg/anet.data cfg/yolov3-anet.cfg  darknet53.conv.74
+conv=$1
+
+echo $@
+
+
+./darknet/darknet detector train cfg/anet.data cfg/yolov3-anet.cfg $conv -gpus 0,1,2,3
+
+# ../data/darknet53.conv.74
