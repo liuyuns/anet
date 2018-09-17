6513984f1ed804547b6e9694680c98a8d3282171
Easton Liu <yun-sheng.liu@hpe.com>
2018-09-17 09:32:09 +0800

    update train_tiny.sh

--- train_tiny.sh
+++ train_tiny.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./darknet/darknet detector train cfg/anet.data cfg/yolov3-anet-tiny.cfg yolov3-tiny.conv.15
+./darknet/darknet detector train cfg/anet_v1.data cfg/anet-tiny.cfg yolov3-tiny.conv.15
