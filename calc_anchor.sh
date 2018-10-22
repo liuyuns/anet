1f6c7468dd376ac28bb5444c0bab4ec1d09da0d4
Easton Liu <easton.ysliu@gmail.com>
2018-10-22 08:24:22 +0000

    update calc_anchors.sh

--- calc_anchor.sh
+++ calc_anchor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./darknet/darknet detector calc_anchors cfg/anet.data -num_of_clusters 6 -width 608 -height 608
+./darknet/darknet detector calc_anchors cfg/anet.data -num_of_clusters 9 -width 416 -height 416
