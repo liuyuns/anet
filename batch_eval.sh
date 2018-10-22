b84a21667c7920700127a265449b290c4aa37a88
Easton Liu <easton.ysliu@gmail.com>
2018-10-22 16:37:52 +0800

    update batch_eval.sh to try resolution from 32*8 to 32* 20.

--- batch_eval.sh
+++ batch_eval.sh
@@ -21,13 +21,15 @@ test_images=( "Android_2_Pro_Cart_Input_Cart.png" "IOS_2_Pro_Cart_Input_Button.p
 
 [ -d output ] || mkdir output
 
+cfg_file=cfg/test-${cfg_name}.cfg
+
 while true
 do
     weight=$( ls backup/${cfg_name}* -t | head -1 )
-    if [ $weight == $last_weight ];
+    if [ "$weight" == "$last_weight" ];
         then 
         echo "Same weight, sleep 1m until next check...."
-        sleep 1m
+        sleep 2m
         continue
     fi
 
@@ -35,17 +37,26 @@ do
 
     base_name=`basename $weight`
 
-    log="output/${base_name}_result.log"
+    log_prefix="output/${base_name}"
 
-    for image in "${test_images[@]}"
-    do
-        echo "Run detection for ${image}"
-        if [ "$eval_script" == "" ];
-          then
-          ./darknet/darknet detector test cfg/anet.data cfg/${cfg_name}.cfg $weight "~/data/$image" -ext_output -dont_show >${log}
-        fi
+    # from 8*32 to 20 * 32, (256-640)
+    for scale in {8..20}; do
+        let "size = ${scale} * 32"
+        echo "Size: ${size}"
+        sed -i s"/width=.*/width=${size}/g" $cfg_file
+        sed -i s"/height=.*/height=${size}/g" $cfg_file
+
+        cat $cfg_file | head -9
+
+        for image in "${test_images[@]}"
+        do
+            echo "Run detection for ${image}"
+            if [ "$eval_script" == "" ];
+            then
+            ./darknet/darknet detector test cfg/anet.data $cfg_file $weight "~/data/$image" -ext_output -dont_show >${log_prefix}_${size}.log
+            fi
+        done
     done
-
 done
 
 
