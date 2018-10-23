2dddb2878f51707c0e325c00ef10b7ca0a36b7d9
Easton Liu <easton.ysliu@gmail.com>
2018-10-23 12:32:33 +0800

    > update analyze_output.sh to support two optional parameters: [pattern] [word] > update train.sh to show usage when no argument is provided.

--- analyze_output.sh
+++ analyze_output.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-word=$1
+pattern=$1
+shift
+if [ "$pattern" == "" ]; then 
+  pattern=* 
+fi
 
+word=$1
 if [ "$word" == "" ]; then
   word=profile
 fi
 
-echo "Showing appearancies of word: '$word' in detection result files."
+echo "Showing appearancies of word: '$word' in detection result files matching $pattern."
 echo ""
 
-for f in output/*; do
+for f in output/*$pattern*; do
   word_count=$( cat $f | grep "$word" | wc -l )
 
   echo "# in $f : $word_count"
--- train.sh
+++ train.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-echo "args: $*"
-
 cfg=$1
 shift
+
+if [ "$cfg" == "" ]; then
+    echo ""
+    echo "Usage:"
+    echo "$0 [cfg-file] [-min_id m](optional, 0 as default) [-max_id n](optional max as default) [-dont_show](optional)"
+    echo ""
+    exit 1
+fi
+
+echo "Args after cfg: $*"
 
 conv=../data/darknet53.conv.74 
 
