4e1392b468893c0a1b87022c738b3ee98f2bf5f2
Easton Liu <easton.ysliu@gmail.com>
2018-10-24 02:01:49 +0000

    > add new class: input

--- run_night.sh
+++ run_night.sh
@@ -8,7 +8,7 @@ if [ ! -f "cfg/$cfg_name.cfg" ]; then
 fi
 
 tmux new -s "train" -d
-tmux send-keys -t "train" "./train.sh cfg/${cfg_name}.cfg -max_id 2 -dont_show" C-m
+tmux send-keys -t "train" "./train.sh cfg/${cfg_name}.cfg -dont_show" C-m
 tmux send-keys -t "train" "sudo shutdown -r +15" C-m
 
 tmux new -s "eval" -d
